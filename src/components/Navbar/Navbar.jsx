68070f421e7f0573b8129a43348187eba3b27bde
notsendo <notsendo@localhost.localdomain>
2020-11-20 01:43:38 +0700

    update code

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -2,12 +2,36 @@ import React, { Component } from 'react';
 import './Navbar.scss';
 import image from '../../asset/images/logo-KLK.png';
 import image1 from '../../asset/images/halfhalf.png';
+import image1mb from '../../asset/images/halfhalfmb.png';
 import SearchBox from '../SearchBox/SearchBox';
+
+const breakpoint = 1024;
 
 class Navbar extends Component {
 
     constructor(props) {
         super(props);
+        this.state = {
+            width: window.innerWidth,
+            height: window.innerHeight
+        }
+        this.updateDimensions = this.updateDimensions.bind(this);
+    }
+
+    componentDidMount() {
+        window.addEventListener('resize', this.updateDimensions);
+    }
+
+    componentWillUnmount() {
+        window.removeEventListener('resize', this.updateDimensions);
+    }
+
+    componentDidUpdate() {
+        // console.log(this.state.width)
+    }
+
+    updateDimensions() {
+        this.setState({ width: window.innerWidth, height: window.innerHeight });
     }
 
     render() {
@@ -15,7 +39,7 @@ class Navbar extends Component {
             <div className="navbar">
                 <div className="search-bar-and-logo">
                     <div className="logo-container">
-                        <img src={image1} alt=""/>
+                        <img src={this.state.width > breakpoint ? image1 : image1mb} alt=""/>
                     </div>
                     <div className="search-box-container">
                         <SearchBox data={this.props.data} />
